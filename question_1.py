89e382056f4ce3be633863c41fba6dd85b7a0d11
jhulter <79738742+jhulter@users.noreply.github.com>
2024-06-23 16:33:36 -0700

    Update question_1.py

--- question_1.py
+++ question_1.py
@@ -1,7 +1,15 @@
 import os
 
+# The commented out variable is how I would probably set it up but since I don't know the system
+# that this will be running on I can't really specify a path... So the way I coded it will show the
+# current running directory...
+
+# Path = input("Enter the directory path you want to print the contents of: ")
+
 def list_directory_contents():
     print(os.listdir())
 
-# List and print all files and subdirectories in the given path
-list_directory_contents()
+try:
+    list_directory_contents()
+except NotADirectoryError as err:
+    print("Directory does not exist")
