0f10ee45e393e680e6b0ee91125c44e33aca0e67
jhulter <79738742+jhulter@users.noreply.github.com>
2024-06-25 10:58:40 -0700

    Add files via upload

--- question_1.py
+++ question_1.py
@@ -4,10 +4,11 @@ import os
 # that this will be running on I can't really specify a path... So the way I coded it will show the
 # current running directory...
 
-# Path = input("Enter the directory path you want to print the contents of: ")
+
 
 def list_directory_contents():
-    print(os.listdir())
+    path = input("Enter the directory path you want to print the contents of: ")
+    print(os.listdir(path))
 
 try:
     list_directory_contents()
